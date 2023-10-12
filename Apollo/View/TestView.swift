3b2e9468f2af6660fc7c8bd80a91c8abedc90145
Kavin <01kavin01@gmail.com>
2023-10-12 11:06:43 -0500

    made a dummy file

---
+++ Apollo/View/TestView.swift
@@ -0,0 +1,21 @@
+//
+//  TestView.swift
+//  Apollo
+//
+//  Created by Kavin Sitsabeshon on 10/12/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    TestView()
+}
