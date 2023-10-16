f5653321c42a589fb09aa2faba78b19ce10925db
andyhsu1 <andyhsu1991@utexas.edu>
2023-10-16 00:19:11 -0500

    login screen front end

---
+++ Apollo/View/LoginView.swift
@@ -0,0 +1,90 @@
+//
+//  LoginView.swift
+//  Apollo
+//
+//  Created by Andy Hsu on 10/13/23.
+//
+
+import SwiftUI
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
+struct LoginView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    
+    let backgroundColor = Color(red: 148/255, green: 172/255, blue: 255/255)
+    
+    
+    var body: some View {
+        backgroundColor.overlay(
+            VStack(alignment: .center, spacing: 25) {
+                Text("APOLLO")
+                    .font(Font.custom("Bodoni 72 Smallcaps", size: 52))
+                
+                VStack(alignment: .leading) {
+                    Text("Username/Email")
+                    TextField("username", text: $username)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.top, 10)
+                    Text("Password")
+                        .padding(.top, 10)
+                    SecureField("password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.top, 10)
+                    Button("Forgot password") {
+                        print("forgot password")
+                    }
+                    .controlSize(.small)
+                }
+                .padding(.top, 40)
+                
+                Button("Login") {
+                    // [TODO] need to add the action for login button
+                    print("login")
+                }
+                .frame(width: 150, height: 50)
+                .background(Color(red: 1, green: 1, blue: 1))
+                .clipShape(Capsule())
+                .padding(.top, 50.0)
+                
+                Text("or")
+                
+                Button("Create account") {
+                    // [TODO] need to add the action for create account button
+                    print("go to create account view")
+                }
+                .frame(width: 150, height: 50)
+                .background(Color(red: 1, green: 1, blue: 1))
+                .clipShape(Capsule())
+                
+                // replace this with Google sign on button
+                Button("Sign in with Google") {
+                    print("Google sign on")
+                }
+                .frame(width: 150, height: 50)
+                .background(Color(red: 1, green: 1, blue: 1))
+                .clipShape(Capsule())
+                
+                
+                Spacer()
+                
+            }
+            .padding(.top, 175.0)
+            .frame(width: 300)
+            
+        )
+        .ignoresSafeArea()
+        
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
