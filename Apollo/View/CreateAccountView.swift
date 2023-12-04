c74029ac53762bd8bb6364270ed647373bdd1b1f
andyhsu1 <andyhsu1991@utexas.edu>
2023-12-03 22:35:20 -0600

    split create account view into two separate views

--- Apollo/View/CreateAccountView.swift
+++ Apollo/View/CreateAccountView.swift
@@ -24,19 +24,13 @@ struct CreateAccountView: View {
     @State private var selectedOptions: Set<String> = []
     @Binding var userSelectedInterests: Bool
     @EnvironmentObject var nightModeManager: NightModeManager
-    let haptics = UINotificationFeedbackGenerator()
-    
-    let backgroundColor = Color(red: 224/255, green: 211/255, blue: 175/255)
     
     var body: some View {
-        content
-    }
-    
-    var content: some View {
-                
         VStack(alignment: .center, spacing: 25) {
             
+            
             VStack(alignment: .leading) {
+                Spacer()
                 
                 Text("Full Name")
                 TextField("", text: $fullName)
@@ -53,6 +47,96 @@ struct CreateAccountView: View {
                 SecureField("", text: $password)
                     .textFieldStyle(.roundedBorder)
                 
+                Spacer()
+                
+            }
+            .frame(width: 320)
+            .ignoresSafeArea(.keyboard)
+            
+            
+            VStack {
+                Spacer()
+                
+                NavigationLink("Next") {
+                    DobLocationOccupationView(fullName: $fullName, password: $password, email: $email, selectedOptions: $selectedOptions, userSelectedInterests: $userSelectedInterests)
+                }
+                .modifier(ButtonModifier())
+                
+                Spacer()
+            }
+            .frame(width: 320)
+            .ignoresSafeArea(.keyboard)
+            
+        }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+        .padding(.top, 80)
+        .padding(.bottom, 70)
+        .preferredColorScheme(nightModeManager.isNightMode ? .dark : .light)
+        .background(Theme.appColors)
+        .environment(\.colorScheme, nightModeManager.isNightMode ? .dark : .light)
+    }
+    
+//    var content: some View {
+//
+//
+//    }
+    
+    private func createUser() {
+        Auth.auth().createUser(withEmail: email, password: password) {
+            (authResult,error) in
+                if let error = error as NSError? {
+                    errorMessage = "\(error.localizedDescription)"
+                } else {
+                    errorMessage = ""
+                }
+        }
+    }
+    
+    private func storeUserData() {
+        let userData = [
+            "email": email,
+            "fullName": fullName,
+            "dateOfBirth": dateOfBirth,
+            "location": location,
+            "occupation": occupation,
+            "interests": Array(selectedOptions),
+            "seenArticles": [],
+            "savedArticles": []
+        ] as [String : Any]
+        
+        Firestore.firestore().collection("users").document(email).setData(userData) {
+            error in
+            if let error = error {
+                errorMessage = "\(error)"
+            } else {
+                errorMessage = ""
+            }
+        }
+    }
+}
+
+struct DobLocationOccupationView: View {
+    
+    @Binding var fullName: String
+    @Binding var password: String
+    @Binding var email: String
+    
+    @State private var dateOfBirth = ""
+    @State private var occupation = ""
+    @State private var location = ""
+    @State private var errorMessage = ""
+    @Binding var selectedOptions: Set<String>
+    @Binding var userSelectedInterests: Bool
+    @EnvironmentObject var nightModeManager: NightModeManager
+    let haptics = UINotificationFeedbackGenerator()
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 25) {
+            
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                
                 Text("Date of Birth")
                     .padding(.top, 25)
                 TextField("mm/dd/yyyy", text: $dateOfBirth)
@@ -61,35 +145,30 @@ struct CreateAccountView: View {
                 Text("Location")
                     .padding(.top, 25)
                 
-//                        LocationButton(.currentLocation) {
-//
-//                        }
-//                        .tint(.white)
-//                        .labelStyle(.iconOnly)
-                
                 TextField("", text: $location)
                     .textFieldStyle(.roundedBorder)
                 
                 Text("Occupation")
                     .padding(.top, 25)
-                    
-//                OccupationListDropdownMenu(selectedOption: $occupation) {
-//                    option in
-//                    self.occupation = option
-//                }
-//                .frame(width: 320)
                 
                 TextField("", text: $occupation)
                     .textFieldStyle(.roundedBorder)
                 
                 Text(errorMessage)
                     .foregroundStyle(.red)
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .fixedSize(horizontal: false, vertical: true)
                 
                 Spacer()
+                
             }
             .frame(width: 320)
+            .ignoresSafeArea(.keyboard)
+            
             
             VStack {
+                Spacer()
+                
                 Button("Create Account") {
                     // TODO add code to segue from this screen to set pfp screen
                     if fullName != "" && email != "" && password != "" && dateOfBirth != "" && location != "" && occupation != "" {
@@ -99,14 +178,18 @@ struct CreateAccountView: View {
                             storeUserData()
                         }
                     } else {
-                        errorMessage = "Please ensure that all fields are filled"
+                        errorMessage = "Please go back and ensure that all fields are filled"
                         self.haptics.notificationOccurred(.warning)
                     }
                     
                 }
                 .modifier(ButtonModifier())
+                
+                Spacer()
             }
             .frame(width: 320)
+            .ignoresSafeArea(.keyboard)
+            
         }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
         .padding(.top, 80)
@@ -147,73 +230,6 @@ struct CreateAccountView: View {
                 errorMessage = ""
             }
         }
-    }
-}
-
-struct OccupationListDropdownMenu: View {
-    
-    @State var presentOptions: Bool = false
-    @Binding var selectedOption: String
-    
-    let setOccupationVar: (_ option: String) -> Void
-    let placeholder: String = ""
-    
-    var body: some View {
-        Button(action: {
-            withAnimation{
-                self.presentOptions.toggle()
-            }
-        }) {
-            HStack {
-                TextField(selectedOption == "" ? placeholder : selectedOption, text: $selectedOption)
-                    .frame(height: 30)
-                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                
-            }
-        }
-        .background(Color.white)
-        .cornerRadius(5)
-        .overlay(alignment: .top) {
-            VStack {
-                if self.presentOptions {
-                    Spacer(minLength: 35)
-                    OccupationListDropdownMenuList(options: occupationsList) {
-                        option in
-                        self.presentOptions = false
-                        self.selectedOption = option
-                        setOccupationVar(option)
-                    }
-                }
-                
-            }
-        }
-    }
-}
-
-struct OccupationListDropdownMenuList: View {
-    
-    let options: [String]
-    
-    let onSelectedAction: (_ option: String) -> Void
-    
-    var body: some View {
-        ScrollView {
-            LazyVStack(alignment: .leading, spacing: 2) {
-                ForEach(options, id: \.self) {
-                    option in
-                    Button (action: {
-                        onSelectedAction(option)
-                    }) {
-                        Text(option)
-                            .foregroundColor(.black)
-                    }
-                }
-            }
-        }
-        .frame(height: 150)
-        .padding(.vertical, 5)
-        .background(.white)
-        .cornerRadius(5)
     }
 }
 
